f22a65270d77587ca9a0dce60ccebaa8f8449f9f
Luxurioust <mrxuri@hotmail.com>
2013-07-08 18:54:25 +0800

    start learning chapter12

---
+++ mysql_connect.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional // EN"
+"http://www.w3c.org/1999/xhtml1-reansitional.dtd">
+<html xmlns="http://www.w3c.org/1999/xhtml" xml:long="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Connect to MySQL</title>
+</head>
+<body>
+<?php // Script 12.1 - mysql_connect.php
+/* This script connects to the MySQL server. */
+
+// Attempt to connect to MySQL and print out messages:
+if ($dbc = mysql_connect('localhost', 'root', 'password')) {
+
+	print '<p>Successfully connected to MySQL!</p>';
+
+	mysql_close(); // Close the connection.
+
+} else {
+
+	print '<p style="color: red; ">Could not connect to MySQL.</p>';
+
+}
+
+?>
+</body>
+</html>
