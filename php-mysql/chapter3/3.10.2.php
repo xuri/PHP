70b1cd1c1bea9a5d97cbda73613b97aec24f41ee
Luxurioust <mrxuri@hotmail.com>
2013-08-31 20:34:20 +0800

    for

--- php-mysql/chapter3/3.10.2.php
+++ php-mysql/chapter3/3.10.2.php
@@ -3,5 +3,17 @@
 	for ($kilometers = 1; $kilometers <= 5; $kilometers++){
 		 printf ("%d kilometers = %f miles <br />", $kilometers, $kilometers*0.62140);
 	}
-
+	// Example Two
+	for ($kilometers = 1;; $kilometers++){
+		if ($kilometers > 5)break;
+		printf("%d kilometers = %f mailes <br />", $kilometers, $kilometers*0.62140);
+	}
+	// Example Thre
+	$kilometers = 1;
+	for (;;){
+		// If $kilometers > 5 break out of the for loop.
+		if ($kilometers > 5) break;
+		printf("%d kilometers = %f mailes <br />", $kilometers, $kilometers*0.62140);
+		$kilometers++;
+	}
 ?>
