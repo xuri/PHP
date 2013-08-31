5d987529c5c8f5539c65e8a1229c5c242c192a7a
Luxurioust <mrxuri@hotmail.com>
2013-08-31 20:46:37 +0800

    foreach

--- php-mysql/chapter3/3.10.2.php
+++ php-mysql/chapter3/3.10.2.php
@@ -3,11 +3,13 @@
 	for ($kilometers = 1; $kilometers <= 5; $kilometers++){
 		 printf ("%d kilometers = %f miles <br />", $kilometers, $kilometers*0.62140);
 	}
+	print'<hr />';
 	// Example Two
 	for ($kilometers = 1;; $kilometers++){
 		if ($kilometers > 5)break;
 		printf("%d kilometers = %f mailes <br />", $kilometers, $kilometers*0.62140);
 	}
+	print'<hr />';
 	// Example Thre
 	$kilometers = 1;
 	for (;;){
@@ -16,4 +18,10 @@
 		printf("%d kilometers = %f mailes <br />", $kilometers, $kilometers*0.62140);
 		$kilometers++;
 	}
+
+	$links = array("www.apress.com","www.php.net","www.apache.org");
+		echo "<b>Online Resources</b>:<br />";
+		foreach($links as $link) {
+			echo "<a href=\"http;//$link\">$link</a><br />";
+		}
 ?>
