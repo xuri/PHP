e474e61adb1855ac8826fe2f7172f6d2f0c6ed6e
Luxurioust <mrxuri@hotmail.com>
2013-08-31 21:11:34 +0800

    break

--- php-mysql/chapter3/3.10.2.php
+++ php-mysql/chapter3/3.10.2.php
@@ -26,4 +26,14 @@
 	foreach ($links as $title => $link) {
 		echo "<a href=\"http://$link\">$title</a><br />";
 	}
+
+	$primes = array(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47);
+	for($count = 1; $count++; $count < 1000){
+		$randomNumber = rand(1,50);
+		if(in_array($randomNumber,$primes)){
+			break;
+		} else {
+			printf("Non-prime number found: %d <br />",$randomNumber);
+		}
+	}
 ?>
