feea22bb7d0ad0a6282688b1cd0c3cafeab3c9d6
Luxurioust <mrxuri@hotmail.com>
2013-09-01 18:22:27 +0800

    continue

--- php-mysql/chapter3/3.10.2.php
+++ php-mysql/chapter3/3.10.2.php
@@ -36,4 +36,10 @@
 			printf("Non-prime number found: %d <br />",$randomNumber);
 		}
 	}
+
+	$usernames = array ("grace","doris","gary","nate","missing","tom");
+	for ($x=0; $x < count($usernames); $x++){
+		if ($username[$x] == "missing") continue;
+		printf("Staff member: %s <br />", $usernames[$x]);
+	}
 ?>
