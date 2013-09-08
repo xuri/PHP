ad914ceb393566e2cbc7d9da083531d0268531ae
Luxurioust <mrxuri@hotmail.com>
2013-09-09 07:53:57 +0800

    5.2.php

--- php-mysql/chapter5/5.1.php
+++ php-mysql/chapter5/5.1.php
@@ -3,7 +3,5 @@
 		"Ohio" => array("population"=>"11,353,140","capital"=>"Columbs"),
 		"Nebraska"=> array("population"=>"1,711,263","capital"=>"Omaha")
 		);
-	$s=$states["Ohio"]["population"];
-	echo "$s";
-
+	echo $states["Ohio"]["population"];
 ?>
---
+++ php-mysql/chapter5/5.2.php
@@ -0,0 +1,8 @@
+<?php
+	$state[0] = "Delaware";
+	echo $state[0];
+
+	$state[1] = "Pennsylvanla";
+	$state[2] = "New Jersey";
+	
+?>
