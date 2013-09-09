90548853cd01ef74a175e0e4176da9667fc8eedb
Luxurioust <mrxuri@hotmail.com>
2013-09-10 07:33:56 +0800

    test array

---
+++ php-mysql/chapter5/5.2.4.php
@@ -0,0 +1,6 @@
+<?php
+	$states = array("Florida");
+	$state = "Ohio";
+	printf("\$states is an array: %s <br />", (is_array($states) ? "TRUE" : "FALSE" ));
+	printf("\$state is an array: %s <br />", (is_array($state) ? "TRUE" : "FALSE" ));
+?>
