b1b7cacc0fd1b98e3179e745189a64c64baf457d
Luxurioust <mrxuri@hotmail.com>
2013-09-10 08:05:15 +0800

    array_key_exists

--- php-mysql/chapter5/5.4.1.php
+++ php-mysql/chapter5/5.4.1.php
@@ -1,5 +1,7 @@
 <?php
-	$state = "Ohio";
-	$states = array("California","Hawaii","Ohio","New York");
-	if(in_array($state,$states)) echo "Not to worry, $state is smook-free!";
+	$state["Delaware"] = "December 7, 1787";
+	$state["Pennsylvania"] = "December 12, 1787";
+	$state["Ohio"] = "March 1, 1803";
+	if (array_key_exists("Ohio", $state))
+		printf("Ohio joined the Union on %s", $state["Ohio"]);
 ?>
