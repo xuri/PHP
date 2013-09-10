2eae205edf42fc3c0ecd33474590469facc159a3
Luxurioust <mrxuri@hotmail.com>
2013-09-10 08:30:52 +0800

    array_keys

--- php-mysql/chapter5/5.4.1.php
+++ php-mysql/chapter5/5.4.1.php
@@ -1,7 +1,7 @@
 <?php
-	$state["Ohio"] = "March 1";
-	$state["Delaware"] = "December 7";
-	$state["Pennsylvania"] = "December 12";
-	$founded = array_search("December 7", $state);
-	if ($founded) printf ("%s was founded on %s.", $founded, $state[$founded]);
+	$state["Delaware"] = "December 7, 1787";
+	$state["Pennsylvania"] = "December 12, 1787";
+	$state["New Jersey"] = "December 18, 1787";
+	$keys = array_keys($state);
+	print_r($keys);
 ?>
