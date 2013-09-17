cd07a3199f887356ad63c8f289b0a5f2689fdfef
Luxurioust <mrxuri@hotmail.com>
2013-09-17 21:22:45 +0800

    add a 5.7.3.php

---
+++ php-mysql/chapter5/5.7.3.php
@@ -0,0 +1,9 @@
+<?php
+	$grades = array(42,98,100,43,12);
+	sort($grades);
+	print_r($grades);
+	print '<hr/>';
+	$states = array("OH" => "Ohio", "CA" => "California", "MD" => "Maryland");
+	sort($states);
+	print_r($states);
+?>
