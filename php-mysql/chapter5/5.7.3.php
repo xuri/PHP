0a92a90c0454aee21f744aa874dde03779fbb266
Luxurioust <mrxuri@hotmail.com>
2013-09-18 16:37:57 +0800

    update

--- php-mysql/chapter5/5.7.3.php
+++ php-mysql/chapter5/5.7.3.php
@@ -1,11 +1,5 @@
 <?php
-	$grades = array(42,98,100,43,12);
-	sort($grades);
-	print_r($grades);
-	print '<hr/>';
-	$states[0] = "Delaware";
-	$states[1] = "Pennsylvania";
-	$states[2] = "New Jersey";
-	asort($states);
+	$states = array("Ohio","Florida","Massachusetts","Montana");
+	rsort($states);
 	print_r($states);
 ?>
