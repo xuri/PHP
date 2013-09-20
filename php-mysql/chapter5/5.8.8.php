f52cd2ce7ddc23416e536da33d377ea85e8568b6
Luxurioust <mrxuri@hotmail.com>
2013-09-20 11:41:58 +0800

    求数组的差集

---
+++ php-mysql/chapter5/5.8.8.php
@@ -0,0 +1,7 @@
+<?php
+	$array1 = array("OH","CA","NY","HI","CT");
+	$array2 = array("OH","CA","HI","NY","IA");
+	$array3 = array("TX","MD","NE","OH","HI");
+	$diff = array_diff($array1, $array2, $array3);
+	print_r($diff);
+?>
