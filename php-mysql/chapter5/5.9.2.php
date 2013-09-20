7d16a18aab1fe090df7f663a5e8fca7b82ce6723
Luxurioust <mrxuri@hotmail.com>
2013-09-20 15:35:15 +0800

    array_chunk() function

--- php-mysql/chapter5/5.9.2.php
+++ php-mysql/chapter5/5.9.2.php
@@ -2,7 +2,12 @@
 	$cards = array("jh","ks","jd","qh","qs","qd","qc","kh","ks","kd","kc","ah","as","ad","ac");
 	// ahuffle the cards
 	shuffle($cards);
-	print_r($cards);
+	
+	// Use array_chunk() to divide the cards into four equal "hands"
+	$hands = array_chunk($cards, 4);
+
+	print_r($hands);
+	print"<hr />";
 
 	$grades = array(42, "hello", 42);
 	$total = array_sum($grades);
