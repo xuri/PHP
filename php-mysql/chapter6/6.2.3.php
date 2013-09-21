6c40ac71d727ddf6b94b24a7774c9cee7a426510
Luxurioust <mrxuri@hotmail.com>
2013-09-21 20:06:23 +0800

    扩展类

--- php-mysql/chapter6/6.2.3.php
+++ php-mysql/chapter6/6.2.3.php
@@ -3,10 +3,13 @@
 		var $name;
 		function __set($propName, $propValue)
 		{
-			echo "Nonexistent variable: \$$propName!";
+			$this->$propName = $propValue;
 		}
 	}
 	$employee = new Employee();
 	$employee->name = "Mario";
 	$employee->title = "Executive Chef";
+	echo "Name: ".$employee->name;
+	echo "<br />";
+	echo "Title: ".$employee->title;
 ?>
