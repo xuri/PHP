6edc33939544ba88442c2673ec5cd4bd55d49d4b
Luxurioust <mrxuri@hotmail.com>
2013-09-25 18:52:32 +0800

    public method

---
+++ php-mysql/chapter6/6.2.5.php
@@ -0,0 +1,10 @@
+<?php
+	class math_functions
+	{
+		const PI = '3.14159265';
+		const E = '2.7182818284';
+		const EULER = '0.5772156649';
+		// define other constants and methods here...
+	}
+	echo math_functions::PI;
+?>
---
+++ php-mysql/chapter6/6.2.6.php
@@ -0,0 +1,18 @@
+<?php
+	class Visitors
+	{
+		public function greetVisitor()
+		{
+			echo "Hello<br />";
+		}
+
+		function sayGoodbye()
+		{
+			echo "Goodbye<br />";
+		}
+	}
+
+	Visitors::greetVisitor();
+	$visitor = new Visitors();
+	$visitor->sayGoodbye();
+?>
