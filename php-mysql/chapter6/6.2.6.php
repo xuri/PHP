cc481627413aa51ef15d7f5e9154c831187fa1e1
Luxurioust <mrxuri@hotmail.com>
2013-09-25 19:47:35 +0800

    protected method

--- php-mysql/chapter6/6.2.6.php
+++ php-mysql/chapter6/6.2.6.php
@@ -15,4 +15,22 @@
 	Visitors::greetVisitor();
 	$visitor = new Visitors();
 	$visitor->sayGoodbye();
+
+	print"<hr />";
+
+	class Employee
+	{
+		private $ein;
+		function __construct($ein)
+		{
+			if ($this->verifyEIN($ein)) {
+				echo "EIN verified. Finish";
+			}
+		}
+		protected function verifyEIN($ein)
+		{
+			return TRUE;
+		}
+	}
+	$employee = new Employee("123-45-6789");
 ?>
