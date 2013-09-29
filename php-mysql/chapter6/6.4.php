22ee26995dd1eb83fce292de09462ec9d348e115
Luxurioust <mrxuri@hotmail.com>
2013-09-29 19:13:54 +0800

    静态类成员

---
+++ php-mysql/chapter6/6.4.php
@@ -0,0 +1,24 @@
+<?php
+	class Visitor
+	{
+		private static $visitors = 0;
+		function __construct()
+		{
+			self::$visitors++;
+		}
+
+		static function getVisitors()
+		{
+			return self::$visitors;
+		}
+
+	}
+	/* Instantiate the Visitor class. */
+	$visits = new Visitor();
+
+	echo Visitor::getVisitors()."<br />";
+	/* Instantiate another Visitor class. */
+	$visitor2 = new Visitor();
+
+	echo Visitor::getVisitors()."<br />";
+?>
