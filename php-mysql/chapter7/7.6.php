9d46eaebcb47e76648b46146af9c18bea2db3fb4
Luxurioust <mrxuri@hotmail.com>
2013-10-06 11:23:14 +0800

    RemoveProfanity function

--- php-mysql/chapter7/7.6.php
+++ php-mysql/chapter7/7.6.php
@@ -4,5 +4,14 @@
 			// Trim white space and capitalize first word
 			return ucfirst(trim($text));
 		}
+
+		function RemoveProfanity($text) {
+			$badwords = array("idiotic" => "shortsighted",
+								"moronic" => "inreasonable",
+								"insane" => "illogical");
+
+			// Remove bad words
+			return strtr($text, $badwords);
+		}
 	}
 ?>
