859a2b28714223d0289d78eb9492a6a9c911d99a
Luxurioust <mrxuri@hotmail.com>
2013-10-06 11:33:36 +0800

    OOP Advance Feature

--- php-mysql/chapter7/7.6.php
+++ php-mysql/chapter7/7.6.php
@@ -14,4 +14,28 @@
 			return strtr($text, $badwords);
 		}
 	}
+
+	// include "Library.php";
+	// include "Data.php";
+	
+	// Instantiate the Library's Clean  class
+	$filter = new Library::Clean();
+
+	// Instantiate the DataFilter's Clean class
+	$profanity = new DataFilter::Clean();
+
+	// Create a book title
+	$title = "the idiotic sun also rises";
+
+	// Output the title before filtering occurs
+	printf("Title before filters: %s <br />", $title);
+
+	// Remove profanity from the title
+	$title = $profanity->RemoveProfanity($title);
+
+	printf("Title after Data::Clean: %s <br />", $title);
+	// Remove white space and capitalize
+	$title = $filter->FilterTitle($title)
+
+	printf("Title after Library::Clean: %s <br />", $title);
 ?>
