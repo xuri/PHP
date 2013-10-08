66a254b4a88f02332f8165b055794a4584c08cfc
Ri Xu <mrxuri@hotmail.com>
2013-10-08 20:37:52 +0800

    以区分大小写的方式搜索

--- php-mysql/chapter9/9.1.2.php
+++ php-mysql/chapter9/9.1.2.php
@@ -3,5 +3,22 @@
 	if (ereg("([^a-z])",$username))
 		echo "Username must be all lowercase!";
 	else
-		echo "Username is all lowercase";	
+		echo "Username is all lowercase";
+
+	echo "<br /><hr />";
+
+	$url = "http://www.apress.com";
+
+	// Break $url down into three distinct pieces:
+	// "http://www", "appress", and "com"
+	$parts = ereg("^(http://www)\.([[:alnum:]]+)\.([[:alnum:]]+)", $url, $regs);
+
+	echo $regs[0];		// output the entire string "http://www.appress.com"
+	echo "<br />";
+	echo $regs[1];		// outputs "http://www"
+	echo "<br />";
+	echo $regs[2];		// outputs "apress"
+	echo "<br />";
+	echo $regs[3];		// outputs "com"
+
 ?>
