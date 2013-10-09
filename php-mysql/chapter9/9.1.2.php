37cbb0f42144853830435519f795b7025a145929
Luxurioust <mrxuri@hotmail.com>
2013-10-09 18:25:27 +0800

    ereg_replace()

--- php-mysql/chapter9/9.1.2.php
+++ php-mysql/chapter9/9.1.2.php
@@ -28,4 +28,12 @@
 		echo "Invalid password!";
 	else 
 		echo "Valid password!";
+
+	echo "<br /><hr />";
+
+	$url = "Apress (http://www.apress.com)";
+	$url = ereg_replace("http://([a-zA-Z0-9./-]+)([a-zA-Z/]+)",
+						"<a href=\"\\0\">\\0</a>",$url);
+	echo $url;
+	// Displays Apress (<a href="http://www.apress.com">http://www.apress.com</a>)
 ?>
