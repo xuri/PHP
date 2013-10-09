12bdfc27e02bb4583af65eff459930f77d27e7f9
Luxurioust <mrxuri@hotmail.com>
2013-10-09 20:17:31 +0800

    搜索模式

--- php-mysql/chapter9/9.1.3.php
+++ php-mysql/chapter9/9.1.3.php
@@ -2,4 +2,9 @@
 	$foods = array("pasta", "steak", "fish", "potatoes");
 	$food = preg_grep("/^p/", $foods);
 	print_r($food);
+
+	echo "<hr />";
+	
+	$line = "vim is the greatest word processor ever created!";
+	if (preg_match("/\bVim\b/i", $line, $match)) print "Match found!";
 ?>
