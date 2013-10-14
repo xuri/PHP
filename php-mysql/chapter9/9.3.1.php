18d97876cdb81bf447a27ac8d2cfcf8e491aeb15
Luxurioust <mrxuri@hotmail.com>
2013-10-14 20:33:46 +0800

    根据预定义的定界符分解字符串

--- php-mysql/chapter9/9.3.1.php
+++ php-mysql/chapter9/9.3.1.php
@@ -11,4 +11,14 @@
 		// Don't include the first arguement in sybsequent calls.
 		$tokenized = strtok($tokens);
 	}
+
+	echo "<hr />";
+
+	$summary = <<< summary
+	In the latest installment of the ongoing Developer.com PHP series,
+	I discuss the many improvements and additions to
+	<a href="http://www.php.net">PHP 5's</a> object-oriented architecture.
+summary;
+	$words = sizeof(explode(' ',strip_tags($summary)));
+	echo "Total words in summary: $words";
 ?>
