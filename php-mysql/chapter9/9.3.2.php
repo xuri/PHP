d6faeb688ec496627b7ea2de8bf530d06f10ab58
Luxurioust <mrxuri@hotmail.com>
2013-10-15 21:08:41 +0800

    字符串中单词计数

--- php-mysql/chapter9/9.3.2.php
+++ php-mysql/chapter9/9.3.2.php
@@ -7,4 +7,16 @@
 	foreach ($chart as $letter => $frequency) {
 		echo "Character ".chr($letter)." appers $frequency times<br />";
 	}
+
+	echo "<hr />";
+
+	$summary = <<< summary
+	In the latest installment of the ongoing Developer.com PHP series,
+	I discuss the many improvements and additions to PHP 5's
+	object-oriented architecture.
+summary;
+	
+	$words = str_word_count($summary,2);
+	$frequency = array_count_values($words);
+	print_r($frequency);
 ?>
