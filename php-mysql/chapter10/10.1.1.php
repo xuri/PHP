c6812a9bd2549cccb9e2a7254a16d6c8d2982ac1
Luxurioust <mrxuri@hotmail.com>
2013-10-16 21:31:50 +0800

    get more about path info

--- php-mysql/chapter10/10.1.1.php
+++ php-mysql/chapter10/10.1.1.php
@@ -4,4 +4,12 @@
 	printf("Filename sans extension: %s <br />", basename($path, " .txt"));
 
 	printf ("Directory path: %s", dirname($path));
+
+	echo "<hr />";
+	
+	$pathinfo = pathinfo("/home/www/htdocs/book/chapter10/index.html");
+	printf("Dir name: %s <br />", $pathinfo[dirname]);
+	printf("Base name: %s <br />", $pathinfo[basename]);
+	printf("Extension: %s <br />", $pathinfo[extension]);
+
 ?>
