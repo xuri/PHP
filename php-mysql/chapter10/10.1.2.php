2271444d0ec0f50b3bf239880fe7d945f44ecb03
Luxurioust <mrxuri@hotmail.com>
2013-10-16 22:08:35 +0800

    get directory size

--- php-mysql/chapter10/10.1.2.php
+++ php-mysql/chapter10/10.1.2.php
@@ -21,4 +21,39 @@
 
 	printf ("Partition: %s (Allocated: %.2f MB. Used: %.2f MB.)", 
 		$partition, $totalSpace, $usedSpace);
+
+	echo "<hr />";
+
+	function directory_size($directory) {
+
+		$directorySize = 0;
+
+		// Open the directory and read its contents.
+		if ($dh = @opendir($directory)) {
+
+			// Iterate thought each directory entry.
+			while (($filename = readdir ($dh))) {
+
+				// Filter out some of the unwanted directory entries.
+				if ($filename != "." && $filename != "..")
+				{
+					// File, so determine size and add to total.
+					if (is_file($directory."/".$filename))
+						$directorySize += filesize($directory."/".$filename);
+
+					// New directory, so initiate recursion. */
+					if (is_dir($directory."/".$filename))
+						$directorySize += directory_size($directory."/".$filename);
+				}
+			}
+		}
+
+		@closedir($dh);
+		return $directorySize;
+
+	} #end directory_size()
+
+	$directory = "/var/www/php/php-mysql/chapter10/";
+	$totalSize = round((directory_size($directory) / 1048576), 2);
+	echo "Directory $directory: ".$totalSize. "kb.";
 ?>
