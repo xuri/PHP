d3c86b2523568f8588fa62edb246b5393828bf79
Luxurioust <mrxuri@hotmail.com>
2013-10-16 21:28:15 +0800

    获取路径的目录

--- php-mysql/chapter10/10.1.1.php
+++ php-mysql/chapter10/10.1.1.php
@@ -2,4 +2,6 @@
 	$path = "/home/www/data/users.txt";
 	printf("Filename: %s <br />", basename($path));
 	printf("Filename sans extension: %s <br />", basename($path, " .txt"));
+
+	printf ("Directory path: %s", dirname($path));
 ?>
