85336957628dd862d6e964be04cb4ac5e06c2a32
Luxurioust <mrxuri@hotmail.com>
2013-10-17 17:35:03 +0800

    file inode last change

--- php-mysql/chapter10/10.1.3.php
+++ php-mysql/chapter10/10.1.3.php
@@ -1,4 +1,9 @@
 <?php
 	$file = "/var/www/php/php-mysql/chapter10/10.1.3.php";
 	printf("File last accessed: %s", date("m-d-y g:i:sa", fileatime($file)));
+
+	echo "<hr />";
+
+	printf("File inode last changed: %s", date("m-d-y g:i:sa",
+		filectime($file)));
 ?>
