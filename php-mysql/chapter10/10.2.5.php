92d25cd3469b1cd15efa9ae1d01dbcdd4fa20706
Luxurioust <mrxuri@hotmail.com>
2013-10-18 21:57:32 +0800

    从输入中剔除标记

--- php-mysql/chapter10/10.2.5.php
+++ php-mysql/chapter10/10.2.5.php
@@ -44,4 +44,25 @@
 
 	// Close the handle
 	fclose($fh);
+
+	echo "<hr />";
+
+	// Build list of accepeable tags
+	$tags = "<h2><h3><p><b><a><img>";
+
+	// Open the article, and read its contents.
+	$fh = fopen("article.html", "rt");
+
+	while (!feof($fh)) {
+		$article .= fgetss($fh, 1024, $tags);
+	}
+	// Close the handle
+	fclose($fh);
+
+	// Open the file up in write mode and output its contents.
+	$fh = fopen("article.html", "wt");
+	fwrite($fh, $article);
+
+	// Close the handle
+	fclose($fh);
 ?>
