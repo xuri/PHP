1f1a53f8e584fbbed2bf1d8fd9e51b5ea28dce86
Luxurioust <mrxuri@hotmail.com>
2013-10-22 07:15:41 +0800

    exec() and system() function

---
+++ php-mysql/chapter10/10.3.2.php
@@ -0,0 +1,7 @@
+<?php
+	$outcome = exec("/var/www/php/php-mysql/chapter10/language.pl", $results);
+	foreach ($results as $result) echo $outcome;
+
+	$outcome = system("/var/www/php/php-mysql/chapter10/language.pl",$results);
+	echo $outcome;
+?>
---
+++ php-mysql/chapter10/language.pl
@@ -0,0 +1,5 @@
+#! /usr/bin/perl
+my @language = qw[perl php python java c];
+foreach $language (@language){
+	print $language."<br />";
+}
