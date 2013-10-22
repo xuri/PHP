83282611234d004753be7e92f4f06a6f3a2a7609
Luxurioust <mrxuri@hotmail.com>
2013-10-22 16:55:29 +0800

    date() function

--- php-mysql/chapter12/12.2.2.php
+++ php-mysql/chapter12/12.2.2.php
@@ -10,4 +10,12 @@
 	$monthyear = date("F Y");
 
 	printf ("Today is %s day of %s", $weekday, $daynumber, $monthyear);
+
+	echo "<hr />";
+
+	echo date("Today is 1 the ds day of F Y");
+
+	echo "<hr />";
+
+	echo "The time is ".date("h:i:s");
 ?>
