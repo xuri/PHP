37718aa5e738a53e9a830f8157d5b41856fdd363
Luxurioust <mrxuri@hotmail.com>
2013-10-26 12:16:30 +0800

    PHP CrackLib Extension

---
+++ php-mysql/chapter14/14.4.1.php
@@ -0,0 +1,17 @@
+<?php
+	$pswd = "567hejk39";
+
+	/* Open the directionary. Note that the dictionary
+	filename does NOT include the extension. 
+	*/
+	$dictionary = crack_opendict('/usr/lib/cracklib_dict');
+
+	// Check password for guessability
+	$check = crack_check($dictionary, $pswd);
+
+	// Retrieve outcome
+	echo crack_getlastmessage();
+
+	// Close dictionary
+	crack_closedict($dictionary);
+?>
