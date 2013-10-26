4eff39ee9df94cb70bf5a9372747ea3912a28158
Luxurioust <mrxuri@hotmail.com>
2013-10-26 12:30:05 +0800

    reset user's password

--- php-mysql/chapter14/14.4.2.php
+++ php-mysql/chapter14/14.4.2.php
@@ -18,4 +18,20 @@ email;
 	// Email user password reset options
 	mail($address,"PAssword recovery","$email", "FROM: services@example.com");
 	echo "<p>Instructions regarding resetting your password have been sent to $address</p>";
+
+
+	echo "<hr />";
+
+	// Creat a pseudorandom password five characters in length
+	$pswd = substr(md5(uniqid(rand())), 5);
+
+	// User's hash value
+	$id = $_GET[id];
+
+	// Update the user table with the new password
+	$query = "UPDATE logins SET pswd='$pswd' WHERE hash='$id'";
+	$result = mysql_query($query);
+
+	// Display the new password
+	echo "<p>Your password has been to $password.</p>"
 ?>
