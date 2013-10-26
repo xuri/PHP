af64da8629cf396ea874d721e8c96b90c1a187cb
Luxurioust <mrxuri@hotmail.com>
2013-10-26 17:55:12 +0800

    verify email address

--- php-mysql/chapter16/16.1.1.php
+++ php-mysql/chapter16/16.1.1.php
@@ -2,4 +2,14 @@
 	$recordexists = checkdnsrr("example.com", "ANY");
 	if ($recordexists) echo "The domain name has been reserved. Sorry!";
 	else echo "The domain name is available!";
+
+	echo "<hr />";
+
+	$email = "ceo@example.com";
+	$domain = explode("@",$email);
+
+	$valid = checkdnsrr($domain[1], "ANY");
+
+	if($valid) echo "The domain exists!";
+	else echo "Cannot locate MX record for $domain[1]!";
 ?>
