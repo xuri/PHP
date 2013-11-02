14dcbc0ba5fbf5a563b2f9559dae977a27ed4ffa
Luxurioust <mrxuri@hotmail.com>
2013-11-02 16:20:29 +0800

    session_decode()

--- php-mysql/chapter18/18.3.5.php
+++ php-mysql/chapter18/18.3.5.php
@@ -1,12 +1,12 @@
 <?php
 	// Initiate sessiob annd create a few session variables
 	session_start();
+	$sid = session_id();
 
-	// Set a few session variavles.
-	$_SESSION['username'] = "jason";
-	$_SESSION['loggedon'] = date("M d Y H:i:s");
+	// Encoded data retrieved from database looks like this;
+	// $sessionVars = username|s:5:"jason";loggedon|s:20:"Feb 16 2008 22:32:29";
 
-	// Encode all session data into a single string and return the resulr
-	$sessionVars = session_encode();
-	echo $sessionVars;
+	session_decode($sessionVars);
+
+	echo "User ".$_SESSION['username']." logged on at ".$_SESSION['loggedon'].".";
 ?>
