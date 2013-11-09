040a0283214674f90300228de0791e96c9592b7a
Ri Xu <luxurioust@luxurioust.local>
2013-11-09 21:04:22 +0800

    change to OS X enveriment

--- php-mysql/chapter19/index.php
+++ php-mysql/chapter19/index.php
@@ -4,10 +4,10 @@
 	require('/usr/local/lib/php/Smarty/Smarty.class.php');
 	$smarty = new Smarty();
 
-	$smarty->setTemplateDir('/var/www/php/php-mysql/chapter19/smarty/templates');
-	$smarty->setCompileDir('/var/www/php/php-mysql/chapter19/smarty/templates_c');
-	$smarty->setCacheDir('/var/www/php/php-mysql/chapter19/smarty/cache');
-	$smarty->setConfigDir('/var/www/php/php-mysql/chapter19/smarty/configs');
+	$smarty->setTemplateDir('/Users/luxurioust/Sites/php/php-mysql/chapter19/smarty/templates');
+	$smarty->setCompileDir('/Users/luxurious/Sites/php/php-mysql/chapter19/smarty/templates_c');
+	$smarty->setCacheDir('/Users/luxurioust/Sites/php/php-mysql/chapter19/smarty/cache');
+	$smarty->setConfigDir('/Users/luxurioust/Sites/php/php-mysql/chapter19/smarty/configs');
 
 	// Creat the array
 	$titles[] = array(
