c814236111fff06f6e6397259fcebc5bb8673893
Ri Xu <luxurioust@luxurioust.local>
2013-11-14 19:36:41 +0800

    remove line breaks

--- php-mysql/chapter20/20.3.2.php
+++ php-mysql/chapter20/20.3.2.php
@@ -1,10 +1,4 @@
 <?php
 	$xml = simplexml_load_file("books.xml");
-	foreach($xml->book as $book) {
-		printf("%s is %s.<br />", $book->author, $book->author->attributes());
-	}
-
-	echo "<hr />";
-
-	echo $xml->book[2]->author->attributes();
+	echo htmlspecialchars($xml->asXML());
 ?>
