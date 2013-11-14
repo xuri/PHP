76ddab81ec1c4df91fa9d065347e38118fdb2913
Ri Xu <luxurioust@luxurioust.local>
2013-11-14 19:18:54 +0800

    SimpleXML

--- php-mysql/chapter19/smarty/cache/a9346418e4128cc64527ccfe63e7996edb046008.section2.tpl.php
+++ php-mysql/chapter19/smarty/cache/a9346418e4128cc64527ccfe63e7996edb046008.section2.tpl.php
@@ -11,11 +11,11 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'nocache_hash' => '37535553652821e9c5b13b9-02830347',
   'version' => 'Smarty-3.1.15',
-  'unifunc' => 'content_52821f4532bd68_68083581',
+  'unifunc' => 'content_52822be61289f3_28617836',
   'has_nocache_code' => false,
   'cache_lifetime' => 1800,
 ),true); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_52821f4532bd68_68083581')) {function content_52821f4532bd68_68083581($_smarty_tpl) {?>	<p>Title: Pro PHP<br />
+<?php if ($_valid && !is_callable('content_52822be61289f3_28617836')) {function content_52822be61289f3_28617836($_smarty_tpl) {?>	<p>Title: Pro PHP<br />
 	Author: Kevin McArthur<br />
 	Published: 2008</p>
 	<p>Title: Beginning Python<br />
